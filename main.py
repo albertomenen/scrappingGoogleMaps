8dab3d09b7cc257a774ba48b87b49d5ceca875fd
Alberto Menendez <90271858+albertomenen@users.noreply.github.com>
2022-10-27 13:54:00 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import gspread
 import time 
+import re
 from selenium import webdriver 
 from datetime import datetime
+from selenium.webdriver.common.action_chains import ActionChains
+
+class ScrapearGMaps:
+  
 
 
 driver = webdriver.Chrome(executable_path="/Macintosh HD/Usuarios/albertomenendez/Escritorio/Automatiz/googleMaps/chromedriver 2")
