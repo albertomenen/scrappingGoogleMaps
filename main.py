6dedbceca768160f953583afb577e3ff66c2d482
Alberto Menendez <90271858+albertomenen@users.noreply.github.com>
2022-10-27 13:49:33 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import gspread
 import time 
 from selenium import webdriver 
+from datetime import datetime
 
 
 driver = webdriver.Chrome(executable_path="/Macintosh HD/Usuarios/albertomenendez/Escritorio/Automatiz/googleMaps/chromedriver 2")
